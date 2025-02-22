36592fca571199dadd1a706fecfc9fa786f96901
GDias Linux <diasgeovane7@gmail.com>
2025-02-22 20:19:46 -0300

    Criado o primeiro protótipo da página do jogo

---
+++ src/components/game/dungeon.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <section id="dungeon">
+    <button id="deck">
+      <img src="/src/assets/backs/red.svg" alt="">
+    </button>
+
+    <div>
+      <img class="card" src="/src/assets/blank_card.svg" alt="">
+      <img class="card" src="/src/assets/blank_card.svg" alt="">
+      <img class="card" src="/src/assets/blank_card.svg" alt="">
+      <img class="card" src="/src/assets/blank_card.svg" alt="">
+    </div>
+  </section>
+ </template>
+
+<style scoped>
+#dungeon {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 15vh;
+
+  div {
+    display: flex;
+  }
+}
+#deck {
+  height: fit-content;
+  margin: 2vh;
+  border-style: none;
+  border-radius: 5%;
+  box-shadow: -1px 1px 2px 1px rgba(0, 0, 0, 0.2);
+
+  cursor: pointer;
+  
+  img {
+    height: 20vh;
+    display: block;
+  }
+}
+
+.card {
+  height: 20vh;
+  display: block;
+  margin: 2vh;
+}
+</style>
---
+++ src/components/game/game.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Dungeon from "./dungeon.vue"
+</script>
+
+<template>
+
+  <section>
+    <Dungeon/>
+
+    
+  </section>
+
+</template>
+
+<style scoped>
+
+</style>
