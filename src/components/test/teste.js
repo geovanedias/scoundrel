9d8570d84bfada2946cb1f596572b6dd67dfee6c
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2025-02-22 09:57:52 -0300

    Melhorado a formatação do array de objetos do baralho

--- src/components/test/teste.js
+++ src/components/test/teste.js
@@ -1,63 +1,62 @@
 const baralho = [
-  { id: "♠1", tipo: "cura", valor: 1, imagem: "espadas_1.png" },
-  { id: "♠2", tipo: "cura", valor: 2, imagem: "espadas_2.png" },
-  { id: "♠3", tipo: "cura", valor: 3, imagem: "espadas_3.png" },
-  { id: "♠4", tipo: "cura", valor: 4, imagem: "espadas_4.png" },
-  { id: "♠5", tipo: "cura", valor: 5, imagem: "espadas_5.png" },
-  { id: "♠6", tipo: "cura", valor: 6, imagem: "espadas_6.png" },
-  { id: "♠7", tipo: "cura", valor: 7, imagem: "espadas_7.png" },
-  { id: "♠8", tipo: "cura", valor: 8, imagem: "espadas_8.png" },
-  { id: "♠9", tipo: "cura", valor: 9, imagem: "espadas_9.png" },
-  { id: "♠10", tipo: "cura", valor: 10, imagem: "espadas_10.png" },
-  { id: "♠A", tipo: "cura", valor: 11, imagem: "espadas_a.png" },
-  { id: "♠J", tipo: "cura", valor: 12, imagem: "espadas_j.png" },
-  { id: "♠Q", tipo: "cura", valor: 13, imagem: "espadas_q.png" },
-  { id: "♠K", tipo: "cura", valor: 14, imagem: "espadas_k.png" },
+  { id: 'a2', type: 'damage', value: 2, image: 'a2.png' }, // ♠
+  { id: 'a3', type: 'damage', value: 3, image: 'a3.png' },
+  { id: 'a4', type: 'damage', value: 4, image: 'a4.png' },
+  { id: 'a5', type: 'damage', value: 5, image: 'a5.png' },
+  { id: 'a6', type: 'damage', value: 6, image: 'a6.png' },
+  { id: 'a7', type: 'damage', value: 7, image: 'a7.png' },
+  { id: 'a8', type: 'damage', value: 8, image: 'a8.png' },
+  { id: 'a9', type: 'damage', value: 9, image: 'a9.png' },
+  { id: 'a10', type: 'damage', value: 10, image: 'a10.png' },
+  { id: 'aJ', type: 'damage', value: 12, image: 'aJ.png' },
+  { id: 'aQ', type: 'damage', value: 13, image: 'aQ.png' },
+  { id: 'aK', type: 'damage', value: 14, image: 'aK.png' },
+  { id: 'aA', type: 'damage', value: 11, image: 'aA.png' },
 
-  { id: "♣1", tipo: "dano", valor: 1, imagem: "paus_1.png" },
-  { id: "♣2", tipo: "dano", valor: 2, imagem: "paus_2.png" },
-  { id: "♣3", tipo: "dano", valor: 3, imagem: "paus_3.png" },
-  { id: "♣4", tipo: "dano", valor: 4, imagem: "paus_4.png" },
-  { id: "♣5", tipo: "dano", valor: 5, imagem: "paus_5.png" },
-  { id: "♣6", tipo: "dano", valor: 6, imagem: "paus_6.png" },
-  { id: "♣7", tipo: "dano", valor: 7, imagem: "paus_7.png" },
-  { id: "♣8", tipo: "dano", valor: 8, imagem: "paus_8.png" },
-  { id: "♣9", tipo: "dano", valor: 9, imagem: "paus_9.png" },
-  { id: "♣10", tipo: "dano", valor: 10, imagem: "paus_10.png" },
-  { id: "♣A", tipo: "dano", valor: 11, imagem: "paus_a.png" },
-  { id: "♣J", tipo: "dano", valor: 12, imagem: "paus_j.png" },
-  { id: "♣Q", tipo: "dano", valor: 13, imagem: "paus_q.png" },
-  { id: "♣K", tipo: "dano", valor: 14, imagem: "paus_k.png" },
+  { id: 'c2', type: 'damage', value: 2, image: 'c2.png' }, // ♣
+  { id: 'c3', type: 'damage', value: 3, image: 'c3.png' },
+  { id: 'c4', type: 'damage', value: 4, image: 'c4.png' },
+  { id: 'c5', type: 'damage', value: 5, image: 'c5.png' },
+  { id: 'c6', type: 'damage', value: 6, image: 'c6.png' },
+  { id: 'c7', type: 'damage', value: 7, image: 'c7.png' },
+  { id: 'c8', type: 'damage', value: 8, image: 'c8.png' },
+  { id: 'c9', type: 'damage', value: 9, image: 'c9.png' },
+  { id: 'c10', type: 'damage', value: 10, image: 'c10.png' },
+  { id: 'cJ', type: 'damage', value: 12, image: 'cJ.png' },
+  { id: 'cQ', type: 'damage', value: 13, image: 'cQ.png' },
+  { id: 'cK', type: 'damage', value: 14, image: 'cK.png' },
+  { id: 'cA', type: 'damage', value: 11, image: 'cA.png' },
 
-  { id: "♦1", tipo: "arma", valor: 1, imagem: "ouros_1.png" },
-  { id: "♦2", tipo: "arma", valor: 2, imagem: "ouros_2.png" },
-  { id: "♦3", tipo: "arma", valor: 3, imagem: "ouros_3.png" },
-  { id: "♦4", tipo: "arma", valor: 4, imagem: "ouros_4.png" },
-  { id: "♦5", tipo: "arma", valor: 5, imagem: "ouros_5.png" },
-  { id: "♦6", tipo: "arma", valor: 6, imagem: "ouros_6.png" },
-  { id: "♦7", tipo: "arma", valor: 7, imagem: "ouros_7.png" },
-  { id: "♦8", tipo: "arma", valor: 8, imagem: "ouros_8.png" },
-  { id: "♦9", tipo: "arma", valor: 9, imagem: "ouros_9.png" },
-  { id: "♦10", tipo: "arma", valor: 10, imagem: "ouros_10.png" },
-  { id: "♦A", tipo: "arma", valor: 11, imagem: "ouros_a.png" },
-  { id: "♦J", tipo: "arma", valor: 12, imagem: "ouros_j.png" },
-  { id: "♦Q", tipo: "arma", valor: 13, imagem: "ouros_q.png" },
-  { id: "♦K", tipo: "arma", valor: 14, imagem: "ouros_k.png" },
+  { id: 'd2', type: 'weapon', value: 2, image: 'd2.png' }, // ♦
+  { id: 'd3', type: 'weapon', value: 3, image: 'd3.png' },
+  { id: 'd4', type: 'weapon', value: 4, image: 'd4.png' },
+  { id: 'd5', type: 'weapon', value: 5, image: 'd5.png' },
+  { id: 'd6', type: 'weapon', value: 6, image: 'd6.png' },
+  { id: 'd7', type: 'weapon', value: 7, image: 'd7.png' },
+  { id: 'd8', type: 'weapon', value: 8, image: 'd8.png' },
+  { id: 'd9', type: 'weapon', value: 9, image: 'd9.png' },
+  { id: 'd10', type: 'weapon', value: 10, image: 'd10.png' },
+  { id: 'dJ', type: 'weapon', value: 12, image: 'dJ.png' },
+  { id: 'dQ', type: 'weapon', value: 13, image: 'dQ.png' },
+  { id: 'dK', type: 'weapon', value: 14, image: 'dK.png' },
+  { id: 'dA', type: 'weapon', value: 11, image: 'dA.png' },
 
-  { id: "♥1", tipo: "cura", valor: 1, imagem: "copas_1.png" },
-  { id: "♥2", tipo: "cura", valor: 2, imagem: "copas_2.png" },
-  { id: "♥3", tipo: "cura", valor: 3, imagem: "copas_3.png" },
-  { id: "♥4", tipo: "cura", valor: 4, imagem: "copas_4.png" },
-  { id: "♥5", tipo: "cura", valor: 5, imagem: "copas_5.png" },
-  { id: "♥6", tipo: "cura", valor: 6, imagem: "copas_6.png" },
-  { id: "♥7", tipo: "cura", valor: 7, imagem: "copas_7.png" },
-  { id: "♥8", tipo: "cura", valor: 8, imagem: "copas_8.png" },
-  { id: "♥9", tipo: "cura", valor: 9, imagem: "copas_9.png" },
-  { id: "♥10", tipo: "cura", valor: 10, imagem: "copas_10.png" },
-  { id: "♥A", tipo: "cura", valor: 11, imagem: "copas_a.png" },
-  { id: "♥J", tipo: "cura", valor: 12, imagem: "copas_j.png" },
-  { id: "♥Q", tipo: "cura", valor: 13, imagem: "copas_q.png" },
-  { id: "♥K", tipo: "cura", valor: 14, imagem: "copas_k.png" }
+  { id: 'h2', type: 'heal', value: 2, image: 'h2.png' }, // ♥
+  { id: 'h3', type: 'heal', value: 3, image: 'h3.png' },
+  { id: 'h4', type: 'heal', value: 4, image: 'h4.png' },
+  { id: 'h5', type: 'heal', value: 5, image: 'h5.png' },
+  { id: 'h6', type: 'heal', value: 6, image: 'h6.png' },
+  { id: 'h7', type: 'heal', value: 7, image: 'h7.png' },
+  { id: 'h8', type: 'heal', value: 8, image: 'h8.png' },
+  { id: 'h9', type: 'heal', value: 9, image: 'h9.png' },
+  { id: 'h10', type: 'heal', value: 10, image: 'h10.png' },
+  { id: 'hJ', type: 'heal', value: 12, image: 'hJ.png' },
+  { id: 'hQ', type: 'heal', value: 13, image: 'hQ.png' },
+  { id: 'hK', type: 'heal', value: 14, image: 'hK.png' },
+  { id: 'hA', type: 'heal', value: 11, image: 'hA.png' },
+
+  { id: 'jF', type: 'heal', value: 5, image: 'jF.png' },
+  { id: 'jB', type: 'weapon', value: 5, image: 'jB.png' },
 ];
 
 let deckRestante = baralho.length;
@@ -89,7 +88,7 @@ function aumentarVida(cura) {
   console.log(vida);
 }
 
-function showDungeon (embaralhado) {
+function showDungeon(embaralhado) {
   if (embaralhado.length < 4) {
     console.log('Não há mais cartas no baralho');
     return;
@@ -99,4 +98,3 @@ function showDungeon (embaralhado) {
 }
 // Aprender sobre o método acima
 // ler https://bost.ocks.org/mike/shuffle/
-
