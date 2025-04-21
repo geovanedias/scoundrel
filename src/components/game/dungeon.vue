25089323de3bc691b0e36bc3d4e46649e513a988
GDias Linux <diasgeovane7@gmail.com>
2025-04-20 21:03:00 -0300

    Falha ao adicionar arquivos ao último commit

--- src/components/game/dungeon.vue
+++ src/components/game/dungeon.vue
@@ -9,43 +9,94 @@
       <img src="/src/assets/backs/red.svg" alt="">
     </button>
 
-    <div>
+    <div >
       <img class="card" src="/src/assets/blank_card.svg" alt="">
       <img class="card" src="/src/assets/blank_card.svg" alt="">
       <img class="card" src="/src/assets/blank_card.svg" alt="">
       <img class="card" src="/src/assets/blank_card.svg" alt="">
     </div>
   </section>
+
+  <section id="player">
+    <div id="life">
+      <img  src="/src/assets/heart.svg" alt="">
+      <p>30</p>
+    </div>
+
+    <div id="weapon">
+      <img class="card" src="/src/assets/blank_card.svg" alt="">
+    </div>
+  </section>
+
  </template>
 
 <style scoped>
 #dungeon {
   display: flex;
-  justify-content: space-around;
-  margin-top: 15vh;
+  justify-content: space-between;
+  margin: 5vh 10vw;
+  padding-top: 5vh;
 
   div {
     display: flex;
   }
 }
+
 #deck {
   height: fit-content;
   margin: 2vh;
   border-style: none;
   border-radius: 5%;
-  box-shadow: -1px 1px 2px 1px rgba(0, 0, 0, 0.2);
+  box-shadow: -1px 1px 10px 7px rgba(0, 0, 0, 0.2);
 
   cursor: pointer;
   
   img {
-    height: 20vh;
+    height: 25vh;
     display: block;
   }
 }
 
 .card {
-  height: 20vh;
+  height: 25vh;
   display: block;
   margin: 2vh;
+  box-shadow: -1px 1px 10px 5px rgba(0, 0, 0, 0.2);
 }
+
+#player {
+  display: flex;
+  justify-content: space-between;
+  margin: 5vh 7.5vw;
+  padding: 5vh 0;
+}
+
+#life {
+  display: flex;
+  
+  img {
+    justify-content: center;
+    width: 18vw;
+    filter: drop-shadow(5px 3px 7px rgba(0 0 0 / 0.4));
+  }
+
+  p {
+    display: flex;
+    height: fit-content;
+    position: relative;
+    top: 30%;
+    right: 50%;
+    z-index: 1;
+
+    font-size: 6rem;
+    font-weight: 600;
+    color: white;
+    font-family: "Modern Antiqua", serif;
+  }
+}
+
+#weapon {
+  width: 60.5%;
+}
+
 </style>
